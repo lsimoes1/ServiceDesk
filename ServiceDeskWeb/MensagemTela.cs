6446492c94799f6dd3596d75b31829b6d0bfada4
Luan Simões <luan.simoes1@gmail.com>
2018-12-11 08:56:17 -0200

    Melhoria na mensagem de erro.

---
+++ ServiceDeskWeb/MensagemTela.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace ServiceDeskWeb
+{
+    public class MensagemTela
+    {
+        public void MensagemTela(string Msg)
+        {
+            Page.ClientScript.RegisterStartupScript(System.Type.GetType("System.String"), "script",
+                        "<script> M.toast({html: '" + Msg + "', classes: 'rounded'}) </script>");
+        }
+    }
+}
